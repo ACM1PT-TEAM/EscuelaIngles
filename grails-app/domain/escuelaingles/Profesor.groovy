e7b2866d3324920e505d1fb85e774cb54c68570b
fernando <fernando@ubuntu>
2014-04-04 14:42:49 -0600

    Se modifico gsp de pagina principal, y de la vista de profesor, Moy se la come

--- grails-app/domain/escuelaingles/Profesor.groovy
+++ grails-app/domain/escuelaingles/Profesor.groovy
@@ -11,17 +11,18 @@ class Profesor
     String constancia
     String password
     String usuario
-    //def nivel = [1,2,3,4]
-    //def dias = ["Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"]
-    //int[] horarios = [1,2,3,4,5,6,7]
+    String[] dias
+    String[] horarios
     
     static constraints = 
     {
         nombre(blank:false, nullable:false)
         apellidoPaterno(blank:false, nullable:false)
         apellidoMaterno(blank:false, nullable:false)
-        correoElectronico(blank:false, nullable:false)
-        nivel(inList:["I","II","III","IV","V","VI"])
+        correoElectronico(blank:false, nullable:false,email:true,unique:true)
+        nivel(inList:["I","II","III","IV","V","VI"], blanck:false, nullable:false)
+        dias(inList:["Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"],multiple:true)
+        horarios(inList:["7:00-9:00","9:00-11:00","11:00-13:00","13:00-15:00","15:00-17:00","17:00-19:00","19:00-21:00"],multiple:true)
         password (blank:false,password:true)
         usuario(size: 5..15, blank:false, unique:true)
         video (blank:false)
